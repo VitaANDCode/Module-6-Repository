a0f03ce4d6a10f3ee8bc05ae28d67f4f13b5d15f
17Vector <131187562+17Vector@users.noreply.github.com>
2023-04-28 12:41:42 +0500

    Completed A-star
    Сделали А* 1.0

--- js/astar-algorithm.js
+++ js/astar-algorithm.js
@@ -1,23 +1,29 @@
 // Обработчик кликов
+var size;				// размер таблицы
+var table_matrix = [];	//матричный вид таблицы
+var openList = [];		//открытый список
+var closedList = [];	//закрытый список
+var route_matrix = [];	//матрица стрелок
+var current_td = [];	//текущая клетка
+
+var xn, yn, xf, yf, x, y; // координаты начала, конца, курентовые координаты
+var start_id, finish_id;  // номер старта и конца
+
 document.addEventListener('click', function(elem) 
 {
 	if (elem.target.id == 'crt_btn')
 	{
 		var parrent = document.querySelector('.table-map');
-		var size = document.getElementById("size_table").value;
-
-		var table_matrix = [];
+		size = document.getElementById("size_table").value;
 
 		for (let i = 0; i < size; i++)
 		{
 			table_matrix[i] = [];
 			for (let j = 0; j < size; j++)
 			{
-				table_matrix[i,j] = 'P';	// пустота
+				table_matrix[i][j] = 'P';	// пустота
 			}
 		}
-
-		console.log(table_matrix);
 
 		var table = document.createElement('table');
 		table.setAttribute('id', 'astar_table');
@@ -34,83 +40,134 @@ document.addEventListener('click', function(elem)
 		}
 		parrent.appendChild(table);
 	}
-
-	/*if (elem.target.className == 'table-item' && buttonPaint == true) 
+	if (elem.target.className == 'table-item')
 	{
-		elem.target.setAttribute('style', 'background-color: purple');
-		console.log(elem.target.id, size, elem.target.id/size);
-		console.log(Math.floor(elem.target.id/size), elem.target.id % size);
-		table_matrix[Math.floor(elem.target.id/size), elem.target.id % size] = "S";	// стена
-	}
-
-	if (elem.target.className == 'table-item' && buttonStart == true)
-	{
-		elem.target.setAttribute('style', 'background-color: blue');
-		table_matrix[Math.floor(elem.target.id/size), elem.target.id % size] = "N";	// начало
-		var start_id = elem.target.id;
-	}
-
-	if (elem.target.className == 'table-item' && buttonStop == true)
-	{
-		elem.target.setAttribute('style', 'background-color: red');
-		table_matrix[Math.floor(elem.target.id/size), elem.target.id % size] = "K";	// конец
-		var finish_id = elem.target.id;
-	}*/
-
-	/*if (elem.target.id = 'crt_route')
-	{
-		var route_matrix = [];
-		for (let i = 0; i < size; i++)
-			route_matrix[i] = [];
-		
-		var openList = [];
-		var closedList = [];
-
-		openList.push(start_id);
-		var current_id = start_id;
-
-		while (openList.length != 0 || current_id != finish_id)
+		if (td_color == 3) 
 		{
-			if ((size > Math.floor(current_id/size) - 1 >= 0) && table_matrix[Math.floor(current_id/size) - 1,current_id % size] != "S")
-			{
-				openList.push(current_id - size);
-			}
-			if ((size > Math.floor(current_id/size) + 1 >= 0) && table_matrix[Math.floor(current_id/size) + 1,current_id % size] != "S")
-			{
-				openList.push(current_id + size);
-			}
-			if ((size > current_id % size - 1 >= 0) && table_matrix[Math.floor(current_id/size),current_id % size - 1] != "S")
-			{
-				openList.push(current_id - 1);
-			}
-			if ((size > current_id % size + 1 >= 0) && table_matrix[Math.floor(current_id/size),current_id % size + 1] != "S")
-			{
-				openList.push(current_id + 1);
-			}
-
-			if ((size > Math.floor(current_id/size) - 1 >= 0) && (size > current_id % size - 1 >= 0) && table_matrix[Math.floor(current_id/size) - 1,current_id % size - 1] != "S")
-			{
-				openList.push(current_id - size - 1);
-			}
-			if ((size > Math.floor(current_id/size) - 1 >= 0) && (size > current_id % size + 1 >= 0) && table_matrix[Math.floor(current_id/size) - 1,current_id % size + 1] != "S")
-			{
-				openList.push(current_id - size + 1);
-			}
-			if ((size > Math.floor(current_id/size) + 1 >= 0) && (size > current_id % size - 1 >= 0) && table_matrix[Math.floor(current_id/size) + 1,current_id % size - 1] != "S")
-			{
-				openList.push(current_id + size - 1);
-			}
-			if ((size > Math.floor(current_id/size) + 1 >= 0) && (size > current_id % size + 1 >= 0) && table_matrix[Math.floor(current_id/size) + 1,current_id % size + 1] != "S")
-			{
-				openList.push(current_id + size + 1);
-			}
-
-			closedList.push(current_id);
-			openList = openList.filter(function(f){return f != current_id});
-
-
+			elem.target.setAttribute('style', 'background-color: purple');
+			table_matrix[Math.floor(elem.target.id/size)][elem.target.id % size] = "S";	// стена
 		}
-	}*/
+
+		else if (td_color == 1)
+		{
+			elem.target.setAttribute('style', 'background-color: blue');
+			table_matrix[Math.floor(elem.target.id/size)][elem.target.id % size] = "N";	// начало
+			start_id = elem.target.id;
+		}
+
+		else if (td_color == 2)
+		{
+			elem.target.setAttribute('style', 'background-color: red');
+			table_matrix[Math.floor(elem.target.id/size)][elem.target.id % size] = "K";	// конец
+			finish_id = elem.target.id;
+		}
+	}
+	
+
+	if (elem.target.id == 'crt_route')	// если нажали "построить маршрут"
+	{
+		for (let i = 0; i < size; i++)	// создание матрицы стрелок
+			route_matrix[i] = [];
+
+		openList = [];					// создание открытого и закрытого списков
+		closedList = [];
+
+		xn = Math.floor(start_id/size);	// вычисление координат начала и конца
+		yn = start_id % size;
+		xf = Math.floor(finish_id/size);
+		yf = finish_id % size;
+
+		openList.push([xn, yn, Math.abs(xn - xf)+Math.abs(yn - yf)]);	// заносим в открытый список стартовую точку
+		current_td = [xn, yn, Math.abs(xn - xf)+Math.abs(yn - yf)];		// рассматриваемая ячейка
+		while (!(openList.length == 0 || (current_td[0] == xf && current_td[1] == yf)))		// пока список не пуст или мы не пришли к финишу
+		{
+			x = current_td[0];	// координаты рассматриваемой точки
+			y = current_td[1];
+			// далее смотрим соседей и занесём в открытый список если там не стена или он не в другом списке
+			if ((size > x - 1 && x-1 >= 0) && table_matrix[x - 1][y] != "S" && closedList.indexOf((x-1)*size+y) == -1)
+			{
+				let sum = current_td[2] + 10 + Math.abs(x - 1 - xf)+Math.abs(y - yf);
+				openList.push([x - 1, y, sum]);
+				closedList.push((x-1)*size+y)
+				route_matrix[x-1][y] = [x, y];
+			}
+			if ((size > x + 1 && x + 1 >= 0) && table_matrix[x + 1][y] != "S" && closedList.indexOf((x+1)*size+y) == -1)
+			{
+				let sum = current_td[2] + 10 + Math.abs(x + 1 - xf)+Math.abs(y - yf);
+				openList.push([x + 1, y, sum]);
+				closedList.push((x+1)*size+y)
+				route_matrix[x+1][y] = [x, y];
+			}
+			if ((size > y - 1 && y - 1 >= 0) && table_matrix[x][y - 1] != "S" && closedList.indexOf(x*size+y-1) == -1)
+			{
+				let sum = current_td[2] + 10 + Math.abs(x - xf)+Math.abs(y - 1 - yf);
+				openList.push([x, y - 1, sum]);
+				closedList.push(x*size+y-1)
+				route_matrix[x][y-1] = [x, y];
+			}
+			if ((size > y + 1 && y + 1 >= 0) && table_matrix[x][y + 1] != "S" && closedList.indexOf(x*size+y+1) == -1)
+			{
+				let sum = current_td[2] + 10 + Math.abs(x - xf)+Math.abs(y + 1 - yf);
+				openList.push([x, y + 1, sum]);
+				closedList.push(x*size+y+1)
+				route_matrix[x][y+1] = [x, y];
+			}
+
+			if ((size > x - 1 && x - 1 >= 0) && (size > y - 1 && y - 1 >= 0) && table_matrix[x - 1][y - 1] != "S" && closedList.indexOf((x-1)*size+y-1) == -1)
+			{
+				let sum = current_td[2] + 14 + Math.abs(x - 1 - xf)+Math.abs(y - 1 - yf);
+				openList.push([x - 1, y - 1, sum]);
+				closedList.push((x-1)*size+y-1)
+				route_matrix[x-1][y-1] = [x, y];
+			}
+			if ((size > x - 1 && x - 1 >= 0) && (size > y + 1 && y + 1 >= 0) && table_matrix[x - 1][y + 1] != "S" && closedList.indexOf((x-1)*size+y+1) == -1)
+			{
+				let sum = current_td[2] + 14 + Math.abs(x - 1 - xf)+Math.abs(y + 1 - yf);
+				openList.push([x - 1, y + 1, sum]);
+				closedList.push((x-1)*size+y+1)
+				route_matrix[x-1][y+1] = [x, y];
+			}
+			if ((size > x + 1 && x + 1>= 0) && (size > y - 1 && y - 1 >= 0) && table_matrix[x + 1][y - 1] != "S" && closedList.indexOf((x+1)*size+y-1) == -1)
+			{
+				let sum = current_td[2] + 14 + Math.abs(x + 1 - xf)+Math.abs(y - 1 - yf);
+				openList.push([x + 1, y - 1, sum]);
+				closedList.push((x+1)*size+y-1)
+				route_matrix[x+1][y-1] = [x, y];
+			}
+			if ((size > x + 1 && x + 1 >= 0) && (size > y + 1 && y + 1 >= 0) && table_matrix[x + 1][y + 1] != "S" && closedList.indexOf((x+1)*size+y+1) == -1)
+			{
+				let sum = current_td[2] + 14 + Math.abs(x + 1 - xf)+Math.abs(y + 1 - yf);
+				openList.push([x + 1, y + 1, sum]);
+				closedList.push((x+1)*size+y+1)
+				route_matrix[x+1][y+1] = [x, y];
+			}
+			// переносим рассматриваемую точку в закрытый список, чтобы больше не рассматривать
+			closedList.push(current_td[0]*size+current_td[1]);
+			openList = openList.filter(function(f){if (JSON.stringify(f) != JSON.stringify(current_td)) return f});
+			// ищем минимального соседа в открытом списке
+			let min = Infinity;
+			for (let i = 0; i < openList.length; i++)
+			{
+				if (openList[i][2] < min)
+				{
+					current_td = openList[i];
+					min = current_td[2];
+				}
+			}
+		}
+
+		let i = xf;
+		let j = yf;
+		// цикл для отображения итогового маршрута
+		while (!(i == xn && j == yn))
+		{
+			let temp_i = i, temp_j = j;
+			i = route_matrix[temp_i][temp_j][0];
+			j = route_matrix[temp_i][temp_j][1];
+			document.getElementById(i*size+j).setAttribute('style','background-color: green');
+		}
+		document.getElementById(i*size+j).setAttribute('style','background-color: blue');
+	}
 
 	if (elem.target.id == 'reset_btn')
 	{
@@ -119,6 +176,4 @@ document.addEventListener('click', function(elem)
 	}
 });
 
-var buttonPaint = false;
-var buttonStart = false;
-var buttonStop = false;
+var td_color = 0;
